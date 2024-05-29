e4e28659e5f20726f0945e337da22f2f17bb3ff1
nalbors <nalborski.a@gmail.com>
2024-05-29 21:23:09 +0200

    Update player.h

--- player.h
+++ player.h
@@ -5,13 +5,14 @@
 class player {
 	int X, Y;           // Polozenie pionka
 	int ilosc_scian;    // Ilosc pozostalych scian
-	char nick[50];
+	char nick[50]; # czemu char? mamy 2024 uzyj string
 public:
 	// settery
 	void set_X(int XX);
 	void set_Y(int YY);
 	void set_sciany(int sciany);
 	// gettery
+#do czego sluzy get_X i get_Y clasa player powinna miec obiekt pionek ktory jako property bedzie mial obecja pozycje
 	int get_X();
 	int get_Y();
 	int get_sciany();
